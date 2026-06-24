9c618974b4fbf6349c39085675d20af22f8a7555
devdelorino <delorinodwnvwr@gmail.com>
2026-06-24 22:32:11 +0800

    modified products by separating products in array like tshirts, shorts, hoodie, jeans

--- assets/data/products.js
+++ assets/data/products.js
@@ -1,87 +1,100 @@
 export const products = [
   {
-    id: 'prod-mk8z1a-001',
-    category: 't-shirts',
-    image: 'acdc-classic-logo-with-lightning-bolt-mens-premium-t-shirt.jpg',
-    name: 'AC/DC Classic Logo With Lightning Bolt',
-    price: 799
+    tshirts: [
+      {
+        id: 'prod-mk8z1a-001',
+        category: 't-shirts',
+        image: 'acdc-classic-logo-with-lightning-bolt-mens-premium-t-shirt.jpg',
+        name: 'AC/DC Classic Logo With Lightning Bolt',
+        price: 799
+      },
+      {
+        id: 'prod-x9k2p3-002',
+        image: 'adventure-time-vintage-jake-unisex-crewneck-sweatshirt.jpg',
+        name: 'Adventure Time Vintage Jake',
+        price: 1190
+      },
+      {
+        id: 'prod-7qz9xa-003',
+        image: 'america-est-1776-womens-cropped-t-shirt.jpg',
+        name: 'America Est 1776',
+        price: 650
+      },
+      {
+        id: 'prod-a81m2v-004',
+        image: 'america-land-of-the-free-mens-premium-t-shirt.jpg',
+        name: 'America Land Of The Free',
+        price: 700
+      },
+      {
+        id: 'prod-kp9x7z-005',
+        image: 'america-the-beautiful-est-1776-unisex-poly-cotton-t-shirt.jpg',
+        name: 'America The Beautiful Est 1776',
+        price: 750
+      }
+    ]
   },
   {
-    id: 'prod-x9k2p3-002',
-    image: 'adventure-time-vintage-jake-unisex-crewneck-sweatshirt.jpg',
-    name: 'Adventure Time Vintage Jake',
-    price: 1190
+    shorts:
+      [{
+        id: 'prod-zm2c8v-006',
+        image: 'asterix-obelix-asterix-fist-washed-womens-muscle-tank-top.jpg',
+        name: 'Asterix & Asterix Fist',
+        price: 690
+      },
+      {
+        id: 'prod-t7n4q1-007',
+        image: 'us-flag-mens-organic-t-shirt.jpg',
+        name: 'US Flag',
+        price: 640
+      },
+      {
+        id: 'prod-b9x3md-008',
+        image: 'batman-with-bats-and-red-batman-logo-unisex-oversized-heavyweight-t-shirt.jpg',
+        name: 'Batman With Bats And Red Batman Logo',
+        price: 590
+      },
+      {
+        id: 'prod-h2k8zp-009',
+        image: 'classic-1965-57th-birthday-retro-vintage-mens-premium-t-shirt.jpg',
+        name: 'Classic 1965 - 57th birthday Retro Vintage',
+        price: 620
+      },
+      {
+        id: 'prod-qw7x1a-010',
+        image: 'dc-comics-batman-logo-used-look-mens-hoodie.jpg',
+        name: 'DC Comics Batman Logo Used Look',
+        price: 999
+      }]
   },
   {
-    id: 'prod-7qz9xa-003',
-    image: 'america-est-1776-womens-cropped-t-shirt.jpg',
-    name: 'America Est 1776',
-    price: 650
+    hoodie:
+      [{
+        id: 'prod-lm3v8c-011',
+        image: 'game-of-thrones-swords-of-the-kingdoms-mens-t-shirt.jpg',
+        name: 'Game Of Thrones Swords Of The Kingdoms',
+        price: 890
+      },
+      {
+        id: 'prod-xc9p2n-012',
+        image: 'harry-potter-hogwarts-coat-of-arms-mens-hoodie.jpg',
+        name: 'Harry Potter Hogwarts Coat of Arms',
+        price: 1120
+      }]
   },
   {
-    id: 'prod-a81m2v-004',
-    image: 'america-land-of-the-free-mens-premium-t-shirt.jpg',
-    name: 'America Land Of The Free',
-    price: 700
-  },
-  {
-    id: 'prod-kp9x7z-005',
-    image: 'america-the-beautiful-est-1776-unisex-poly-cotton-t-shirt.jpg',
-    name: 'America The Beautiful Est 1776',
-    price: 750
-  },
-  {
-    id: 'prod-zm2c8v-006',
-    image: 'asterix-obelix-asterix-fist-washed-womens-muscle-tank-top.jpg',
-    name: 'Asterix & Asterix Fist',
-    price: 690
-  },
-  {
-    id: 'prod-t7n4q1-007',
-    image: 'us-flag-mens-organic-t-shirt.jpg',
-    name: 'US Flag',
-    price: 640
-  },
-  {
-    id: 'prod-b9x3md-008',
-    image: 'batman-with-bats-and-red-batman-logo-unisex-oversized-heavyweight-t-shirt.jpg',
-    name: 'Batman With Bats And Red Batman Logo',
-    price: 590
-  },
-  {
-    id: 'prod-h2k8zp-009',
-    image: 'classic-1965-57th-birthday-retro-vintage-mens-premium-t-shirt.jpg',
-    name: 'Classic 1965 - 57th birthday Retro Vintage',
-    price: 620
-  },
-  {
-    id: 'prod-qw7x1a-010',
-    image: 'dc-comics-batman-logo-used-look-mens-hoodie.jpg',
-    name: 'DC Comics Batman Logo Used Look',
-    price: 999
-  },
-  {
-    id: 'prod-lm3v8c-011',
-    image: 'game-of-thrones-swords-of-the-kingdoms-mens-t-shirt.jpg',
-    name: 'Game Of Thrones Swords Of The Kingdoms',
-    price: 890
-  },
-  {
-    id: 'prod-xc9p2n-012',
-    image: 'harry-potter-hogwarts-coat-of-arms-mens-hoodie.jpg',
-    name: 'Harry Potter Hogwarts Coat of Arms',
-    price: 1120
-  },
-  {
-    id: 'prod-vk8z7q-013',
-    image: 'outdoor-boys-logo-mens-ringer-t-shirt.jpg',
-    name: 'Outdoor Boys Logo',
-    price: 520
-  },
-  {
-    id: 'prod-r2m9xa-014',
-    image: 'the-hobbit-characters-around-bilbo-baggins-mens-organic-t-shirt.jpg',
-    name: 'The Hobbit Characters Around Bilbo Baggins',
-    price: 650
+    jeans:
+      [{
+        id: 'prod-vk8z7q-013',
+        image: 'outdoor-boys-logo-mens-ringer-t-shirt.jpg',
+        name: 'Outdoor Boys Logo',
+        price: 520
+      },
+      {
+        id: 'prod-r2m9xa-014',
+        image: 'the-hobbit-characters-around-bilbo-baggins-mens-organic-t-shirt.jpg',
+        name: 'The Hobbit Characters Around Bilbo Baggins',
+        price: 650
+      }]
   }
 ];
